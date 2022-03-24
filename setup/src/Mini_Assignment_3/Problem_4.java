be93cfbfe4a86d69b287ce3490e706b309ee56a2
Mayank Kaushik <mayakaushik187@gmail.com>
2022-03-24 10:26:39 +0530

    DAtes added of depart and return

---
+++ setup/src/Mini_Assignment_3/Problem_4.java
@@ -0,0 +1,30 @@
+package Mini_Assignment_3;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Problem_4 {
+    public static void main(String[] args) {
+        String chromePath = "C:\\Users\\mayakaushik\\Desktop\\selenium\\chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", chromePath);
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        navigate(driver);
+    }
+    public static void navigate(WebDriver driver){
+        final String URL = "https://www.goibibo.com/";
+        driver.get(URL);
+        driver.findElement(By.xpath("//span[text()='Round-trip']")).click();
+        driver.findElement(By.xpath("//input")).sendKeys("New York");
+        driver.findElement(By.xpath("//ul[@id='autoSuggest-list']//child::li[@tabindex='0']//child::div[@class='sc-iAKWXU iyyKqe']")).click();
+        driver.findElement(By.xpath("//div[@class='sc-cidDSM dOEpbS']//child::input")).sendKeys("Seattle");
+        driver.findElement(By.xpath("//span[text()='Departure']//parent::div[@class='sc-bkkeKt gAqCbJ fswFld ']")).click();
+        for(int i = 0 ; i < 3 ; i++){
+            driver.findElement(By.xpath("//span[@class='DayPicker-NavButton DayPicker-NavButton--next']")).click();
+        }
+        driver.findElement(By.xpath("//div[@aria-label='Fri Jun 24 2022']")).click();
+        driver.findElement(By.xpath("//div[@aria-label='Fri Jul 08 2022']")).click();
+    }
+}
