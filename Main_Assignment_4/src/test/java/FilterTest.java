c0b303d552962930fafde76bc7e7a96cfec0b511
Mayank Kaushik <mayakaushik187@gmail.com>
2022-03-25 10:21:38 +0530

    Added Add to cart and remove test

--- Main_Assignment_4/src/test/java/FilterTest.java
+++ Main_Assignment_4/src/test/java/FilterTest.java
@@ -7,8 +7,18 @@ public class FilterTest {
 
     @Test
     public void ClickOnFunnel(){
+        //click on filter
         driver.findElement(By.xpath("//div[@class='right_component']//span//select")).click();
+        //click on price high to low
         driver.findElement(By.xpath("//div[@class='right_component']//span//select//option[4]")).click();
+        //click on add to cart
+        driver.findElement(By.xpath("//div[@class='pricebar']//button[@id='add-to-cart-sauce-labs-backpack']")).click();
+
+    }
+    public void clickRemove(){
+        //click on remove
+        driver.findElement(By.xpath("//div[@id='inventory_container']//div//div//div//div//div[2]//div[2]//button[@id='remove-sauce-labs-fleece-jacket']")).click();
+        //click on add to cart
         driver.findElement(By.xpath("//div[@class='pricebar']//button[@id='add-to-cart-sauce-labs-backpack']")).click();
     }
 }
