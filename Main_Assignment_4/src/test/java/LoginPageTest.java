364177602ee5509ca2076261fdefdabd4a912a65
Mayank Kaushik <mayakaushik187@gmail.com>
2022-03-25 09:38:55 +0530

    Login Failure and Success Test

---
+++ Main_Assignment_4/src/test/java/LoginPageTest.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.devtools.v85.log.Log;
+import org.testng.annotations.Test;
+
+public class LoginPageTest extends SenarioTest{
+    @Test
+    public void LoginFailureTest(){
+        LoginPageClass login = new LoginPageClass();
+        login.logFun("asdad","Asdasd");
+    }
+    @Test
+    public void LoginSuccessTest(){
+        String userName = sheet.getRow(1).getCell(0).getStringCellValue();
+        String password = sheet.getRow(1).getCell(1).getStringCellValue();
+        LoginPageClass login = new LoginPageClass();
+        login.logFun()
+    }
+}
